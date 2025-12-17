1fd072325796ca7d56023d420eefb478aa78ad59
ThangaPreethi-03 <thangapreethi85@gmail.com>
2025-12-17 22:13:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api/orders", require("./routes/orders"));
 app.use("/api/users", require("./routes/users"));
+
 app.use("/api/reviews", require("./routes/reviews"));
 
 app.use("/api/payments", require("./routes/payments"));
@@ -99,4 +100,5 @@ mongoose
     console.error("MongoDB error:", err);
     process.exit(1);
   });
+
 
