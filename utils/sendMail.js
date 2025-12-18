582bd950f5cc0419ae86d22fa8ae04a9c051fa46
ThangaPreethi-03 <thangapreethi85@gmail.com>
2025-12-18 21:11:55 +0530

    Update sendMail.js

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,25 +1,25 @@
 // utils/sendEmail.js
 const nodemailer = require("nodemailer");
-console.log("EMAIL:", process.env.EMAIL);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASSWORD);
+console.log("EMAIL:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.EMAIL,
+    user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
 async function sendEmail(to, subject, html) {
-  if (!process.env.EMAIL || !process.env.EMAIL_PASS) {
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
     console.error("❌ MAIL_USER / MAIL_PASS missing");
     return;
   }
 
   try {
     await transporter.sendMail({
-      from: `"PrimeShop" <${process.env.EMAIL}>`,
+      from: `"PrimeShop" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       html,
